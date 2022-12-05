56034fb7638d17785b365a8a9aea459195b7000f
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-12-05 15:29:34 -0800

    initial project setup complete

---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+import {getFireStore} from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxMqOIGHfZh66QYeJbJLJ9ZdaP-7G8Flk",
+  authDomain: "house-marketplace-app-15dae.firebaseapp.com",
+  projectId: "house-marketplace-app-15dae",
+  storageBucket: "house-marketplace-app-15dae.appspot.com",
+  messagingSenderId: "971659142957",
+  appId: "1:971659142957:web:f6413aa465dcf4dd5d15a4"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFireStore();
